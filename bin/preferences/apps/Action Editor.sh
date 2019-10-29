30039adfeedb375b929912d0e212991ddc18d9b7
Roey Biran <roeybiran@gmail.com>
2019-10-30 00:32:29 +0200

    update Action Editor

--- bin/preferences/apps/Action Editor.sh
+++ bin/preferences/apps/Action Editor.sh
@@ -4,7 +4,6 @@
 defaults write "at.obdev.LaunchBar.ActionEditor" myBundleIdentifier -string "com.roeybiran"
 
 # personalInformationAuthor = "Roey Biran";
-# personalInformationEmailIdentifer = "B6BDEAE5-FB03-4392-911A-8F342F2E2E5E";
 # personalInformationEmailString = "roeybiran@icloud.com";
 # personalInformationWebsite = "https://github.com/roeybiran";
 # preferredScriptLanguage = "Shell Script";
